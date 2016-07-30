b77ff00ea01e2d0b2f02f44469423be952c9ad4d
imakura.kazuhiro <ths50473@outlook.com>
2016-07-30 18:42:50 +0900

    add

---
+++ fgs16_space_ricecake_ui/Assets/Distance.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Distance : MonoBehaviour
+{
+    public Text distanceGUI;
+
+    private float distance = 0.0f;
+
+    //初期化処理
+    void Start ()
+    {
+    }
+	
+	//更新処理(毎フレーム)
+	void Update ()
+    {
+	}
+}
---
+++ fgs16_space_ricecake_ui/Assets/Timer.cs
@@ -0,0 +1,28 @@
+﻿//-----------------------------------------------------------------------------
+//時間経過処理(Timer.cs)
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour
+{
+    public Text timeGUI;
+
+    private float time = 31.0f;
+    
+
+    //初期化処理
+    void Start()
+    {
+        timeGUI.text = time.ToString();
+    }
+
+    //更新処理(毎フレーム)
+    void Update()
+    {
+        //タイム
+        time -= 1f * Time.deltaTime;
+        timeGUI.text = ((int)time).ToString();
+    }
+}
