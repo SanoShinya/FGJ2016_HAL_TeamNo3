60ae0b34718ef3c7e29fb6e14a96b1b8c93adb00
mattyakuri-mu <nogegarasu96@gmail.com>
2016-07-30 18:43:45 +0900

    add project

---
+++ New Unity Project 1/Assets/Buttondecision.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buttondecision : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ( )
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ( )
+    {
+        /*        if(  )
+                {
+        
+                }*/
+        if ( Input.GetMouseButtonDown( 0 ) )
+        {
+            print("左ボタンが押された");
+        }
+	}
+}
