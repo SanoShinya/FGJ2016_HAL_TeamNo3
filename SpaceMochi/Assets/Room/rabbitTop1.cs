3ec95eda032e83ff1f29f56848d97a2edc1d3775
SanoShinya <sano.s@nagisa-inc.jp>
2016-07-31 05:21:12 +0900

    部屋を追加

---
+++ SpaceMochi/Assets/Room/rabbitTop1.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rabbitTop1 : MonoBehaviour {
+	int nTime;
+	void Start()
+	{
+		nTime = 0;
+	}
+
+	/// <summary>
+	/// 表示アニメーション
+	/// </summary>
+	void Update(){
+		nTime++;
+		if (nTime < 50) {
+			this.transform.Rotate (0.0f, 1.5f, 0);
+		} else if (nTime < 100) {
+			this.transform.Rotate (0.0f, -1.5f, 0);
+		} else {
+			nTime = 0;
+		}
+	}
+}
