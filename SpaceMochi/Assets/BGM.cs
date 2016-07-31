4cd662b9314976326a61ac3e03786a147cc04aed
mattyakuri-mu <nogegarasu96@gmail.com>
2016-07-31 13:23:01 +0900

    BGMとSEの追加　2016-0731-13:22

---
+++ SpaceMochi/Assets/BGM.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGM : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ( ) {
+        // BGM再生
+        AudioManager.getInstance.PlayBGM("Title_SOUND_03");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // SE再生
+            AudioManager.getInstance.PlaySE("Koukaon02");
+        }
+	
+	}
+}
