9cc3db9d937e05ab2c2ce847c54c05cfb25f6124
yamashita.kohei <yamashita.kohei>
2016-07-31 09:40:09 +0900

    ぱーてぃくる

---
+++ TUTRIAL/Assets/ParticleScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleScript : MonoBehaviour {
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // パーティクルの再生
+        //GetComponent<ParticleSystem>().Play();
+
+        // パーティクルの停止
+        //GetComponent<ParticleSystem>().Stop();
+	}
+}
